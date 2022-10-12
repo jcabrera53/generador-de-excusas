181e9cabba1996d41f2a66faee645756ff8c53a8
jcabrera53 <juancabrera53@gmail.com>
2022-10-12 03:45:19 +0000

    generador de excusas sin fondo

--- src/app.js
+++ src/app.js
@@ -4,8 +4,24 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+//console.log("Hello Rigo from the console!");
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+  let what = ["comió", "se molestó", "aplastó", "rompió"];
+  let when = [
+    "antes de la clase",
+    "justo a tiempo",
+    "cuando terminé",
+    "durante mi almuerzo",
+    "mientras estaba rezando"
+  ];
+  document.querySelector("#excuse").innerHTML = `${
+    who[getRandomInt(who.length)]
+  } 
+    ${what[getRandomInt(what.length)]} 
+    ${when[getRandomInt(when.length)]}`;
 };
